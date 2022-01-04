e0dd63d6af57face02538712e699fd862d306b28
Praful Goyal <prafulgo@amazon.com>
2022-01-04 21:55:07 +0530

    SignUp module added

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import {Switch, BrowserRouter, Route} from 'react-router-dom'
 import AppLayout from "@awsui/components-react/app-layout";
 import GenerateQRCode from "./components/generateQRCode";
 import ContactUs from "./components/contactUs/contactUs"
+import Login from "./components/login/login"
 import SideNavigation from "@awsui/components-react/side-navigation";
 import "@awsui/global-styles/index.css"
+import { COGNITO } from "./configs/aws";
+import Amplify from "aws-amplify";
 import TopNavigation, {TopNavigationProps} from "@awsui/components-react/top-navigation";
 import Utility = TopNavigationProps.Utility;
+
+Amplify.configure({
+    aws_cognito_region: COGNITO.REGION,
+    aws_user_pools_id: COGNITO.USER_POOL_ID,
+    aws_user_pools_web_client_id: COGNITO.APP_CLIENT_ID,
+});
 
 const Navigation = () => {
     return(
@@ -21,13 +30,15 @@ const Content = () => {
   return(
       <Switch>
         <Route exact path={`/`} component={GenerateQRCode} />
+          <Route exact path={`/login`} component={Login} />
         <Route exact path={`/contact`} component={ContactUs} />
       </Switch>
   )
 }
 
 const serviceIdentity = { href: '/QRCodeGenerator', title: "QR Code Generator" }
-const utilities : Utility = { type:"button", "text":"CONTACT US", "href":"/QRCodeGenerator/contact" }
+const utilities : Utility = { type:"button", text:"CONTACT US", href:"/QRCodeGenerator/contact" }
+const login : Utility = { type:"button", iconName:"user-profile", ariaLabel:"Login", href: "/QRCodeGenerator/login" }
 
 const App = () => {
     const [navigationOpen, setNavigationOpen] = useState(false)
@@ -38,7 +49,7 @@ const App = () => {
               <TopNavigation
                   identity={serviceIdentity}
                   i18nStrings={{ overflowMenuTriggerText: "More" }}
-                  utilities={[utilities]}
+                  utilities={[utilities, login]}
               />
           </div>
           <BrowserRouter basename={'/QRCodeGenerator'}>
---
+++ src/configs/aws.ts
@@ -0,0 +1,5 @@
+export const COGNITO = {
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_MsWWHX9vL",
+    APP_CLIENT_ID: "1e765i8grshodi86s7seha13t0",
+};
